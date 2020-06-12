629697b7b7bf54a3a7a1f6da9df2ca7a8820564d
Papyrustaro <undertale1104@gmail.com>
2020-06-12 19:31:35 +0900

    カメラの位置調整機能追加

--- Assets/Scripts/CameraController/ThirdPersonCameraController.cs
+++ Assets/Scripts/CameraController/ThirdPersonCameraController.cs
@@ -7,8 +7,17 @@ using UnityEngine;
 /// </summary>
 public class ThirdPersonCameraController : MonoBehaviour
 {
+    [SerializeField] private Vector3 defaultPosition = new Vector3(0f, 2f, -5f);
+    [SerializeField] private Vector3 onePersonPosition = new Vector3(0f, 1f, 0.5f);
+
+    [SerializeField] private Vector3 overheadPosition = new Vector3(0f, 15f, -1f);
+    [SerializeField] private Vector3 overheadRotation = new Vector3(85f, 0f, 0f);
+
     [SerializeField] private Transform targetPlayer;
+    private float rotationByMouseForce = 200f;
     private Vector3 targetPositionBeforeFrame;
+
+    public E_CameraViewType CameraViewType { private get; set; } = E_CameraViewType.Default;
     private void Start()
     {
         this.targetPositionBeforeFrame = this.targetPlayer.position;
@@ -23,8 +32,84 @@ public class ThirdPersonCameraController : MonoBehaviour
         {
             float mouseInputX = Input.GetAxis("Mouse X");
             float mouseInputY = Input.GetAxis("Mouse Y");
-            this.transform.RotateAround(this.targetPositionBeforeFrame, Vector3.up, mouseInputX * Time.deltaTime * 200f);
-            this.transform.RotateAround(this.targetPositionBeforeFrame, this.transform.right, mouseInputY * Time.deltaTime * 200f);
+            this.transform.RotateAround(this.targetPositionBeforeFrame, Vector3.up, mouseInputX * Time.deltaTime * this.rotationByMouseForce);
+            if(this.CameraViewType != E_CameraViewType.Overhead) this.transform.RotateAround(this.targetPositionBeforeFrame, this.transform.right, mouseInputY * Time.deltaTime * this.rotationByMouseForce);
         }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            WatchFromPlayerBack();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            WatchDefault();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            WatchOnePerson();
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            WatchFromOverHead();
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの正面を向く(カメラをプレイヤーの背中側に移動)。ゆっくり回転させたい気もする
+    /// 一人称視点では、マウスでのみ向いている方向が変わるべきではある。
+    /// </summary>
+    private void WatchFromPlayerBack()
+    {
+        if(this.CameraViewType == E_CameraViewType.OnePerson)
+        {
+            this.transform.RotateAround(this.targetPlayer.position, Vector3.up, this.transform.rotation.eulerAngles.y - this.targetPlayer.rotation.eulerAngles.y);
+        }
+        else
+        {
+            this.transform.RotateAround(this.targetPlayer.position, Vector3.up, this.targetPlayer.rotation.eulerAngles.y - this.transform.rotation.eulerAngles.y);
+        }
+
+        //上下も戻すとき
+        if(this.CameraViewType != E_CameraViewType.Overhead)
+        {
+            this.transform.RotateAround(this.targetPlayer.position, this.transform.right, this.targetPlayer.rotation.eulerAngles.x - this.transform.rotation.eulerAngles.x);
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーを真上から見る(カメラをプレイヤーの真上に移動)
+    /// </summary>
+    private void WatchFromOverHead()
+    {
+        this.CameraViewType = E_CameraViewType.Overhead;
+        this.transform.position = this.targetPlayer.position + this.overheadPosition;
+        this.transform.rotation = Quaternion.Euler(this.overheadRotation);
+    }
+
+    /// <summary>
+    /// 一人称視点
+    /// </summary>
+    private void WatchOnePerson()
+    {
+        this.CameraViewType = E_CameraViewType.OnePerson;
+        this.transform.position = this.targetPlayer.position + this.onePersonPosition;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+    }
+
+    /// <summary>
+    /// defaultの三人称視点
+    /// </summary>
+    private void WatchDefault()
+    {
+        this.CameraViewType = E_CameraViewType.Default;
+        this.transform.position = this.targetPlayer.position + this.defaultPosition;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+    }
+
+    public enum E_CameraViewType
+    {
+        Default,
+        Overhead,
+        OnePerson,
     }
 }
